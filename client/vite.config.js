6fa4b6f00c64a26204b1a47fe593cbb2c263784f
tametheelephant <70871922+tametheelephant@users.noreply.github.com>
2025-12-04 17:02:56 -0500

    fix: update vite config to handle about/support-us pages

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,9 @@ export default defineConfig({
             input: {
                 main: resolve(__dirname, 'index.html'),
                 login: resolve(__dirname, 'login.html'),
-                dashboard: resolve(__dirname, 'dashboard.html')
+                dashboard: resolve(__dirname, 'dashboard.html'),
+                about: resolve(__dirname, 'about.html'),
+                support_us: resolve(__dirname, 'support-us.html')
             },
         },
     },
